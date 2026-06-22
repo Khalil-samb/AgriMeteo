f77f060cdd4cfb5deed24470512435a508ea5410
khalil-samb <ibrahimakhalilsamb77@gmail.com>
2026-06-22 11:49:18 +0000

    Creation du composant dashboard et du dashboard panel

---
+++ src/app/features/dashbord-panel/dashbord-panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord-panel',
+  imports: [],
+  templateUrl: './dashbord-panel.html',
+  styleUrl: './dashbord-panel.css',
+})
+export class DashbordPanel {}
