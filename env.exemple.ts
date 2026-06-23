82ed25655aaf4fc902e7f0e70f1adf41ea7d0142
BigLineDev0 <alioudiallo.py@gmail.com>
2026-06-23 15:33:03 +0000

    fix: bug cles API integration calcul risque et historique temperature

--- env.exemple.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  openWeatherApiKey: '' // meter ici votre cle api openweather
-};
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../env.exemple';
-
+import { environment } from '../../env';
 
 @Injectable({
   providedIn: 'root'
