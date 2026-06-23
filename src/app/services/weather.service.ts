0314c4d4073812e8caf829962c67991039b3568d
BigLineDev0 <alioudiallo.py@gmail.com>
2026-06-23 15:19:32 +0000

    correction conflits merge

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../env';
+import { environment } from '../../../env.exemple';
 
 
 @Injectable({
